53b4989882ff405bcba85c2c19dbed5191b3a604
Marija Vaskeviciute <2267670v@student.gla.ac.uk>
2018-02-02 13:10:38 +0000

    8.2

--- rango/views.py
+++ rango/views.py
@@ -61,4 +61,4 @@ def add_page(request, category_name_slug):
         else:
             print(form.errors)
     context_dict={'form':form, 'category':category}
-    return render(request, 'rango/add_page.html', context_dict)
+    return render(request, 'rango/add_page.html', context=context_dict)
