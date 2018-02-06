42ba6c214b897d8597a94f6d0f1c38d857903f99
Marija Vaskeviciute <2267670v@student.gla.ac.uk>
2018-02-06 15:35:18 +0000

    9.2

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -84,14 +84,6 @@ DATABASES = {
     }
 }
 
-
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-]
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
